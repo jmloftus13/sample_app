78ca5e4ea090a84b4221b82c3753a2155befe0a0
Jeannine Loftus <jmloftus13@yahoo.com>
2016-12-15 19:57:07 -0500

    ex 3.8 created about & contact pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'static_pages/about'
+
+  get 'static_pages/contact'
+
+ root 'static_pages#index'
+  get 'static_pages/index' #this statement was in the original file
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
