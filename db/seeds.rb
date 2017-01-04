bef5ccb8af81367840b87a9e6f457304aee5c2fc
Jeannine Loftus <jmloftus13@yahoo.com>
2017-01-03 20:58:02 -0500

    ex4.2 added color & price to products

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+#Ruby command
+Product.create(:name => "seed-generated product", :description => "I also created this product without using the HTML form!")
