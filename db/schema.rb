7670d8570053ef343092d848d757cc7f0785deac
Jeannine Loftus <jmloftus13@yahoo.com>
2017-01-06 18:47:52 -0500

    Ex4.2 changed color to colour

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170104013229) do
+ActiveRecord::Schema.define(version: 20170106230912) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 20170104013229) do
     t.string   "image_url"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.string   "color"
+    t.string   "colour"
     t.decimal  "price"
   end
 
