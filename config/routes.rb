e015cab2a10affa7655c4c2346ae0c88b7054ec2
Jeannine Loftus <jmloftus13@yahoo.com>
2017-01-15 17:44:09 -0500

    Ex4.4 created model for Orders

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
  root 'static_pages#landing_page'
   get 'static_pages/index' #this statement was in the original file
+  get 'static_pages/landing_page'
 
   resources :orders, only: [:index, :show, :create, :detroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
