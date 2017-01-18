5c984a5bc7ec1bde67509567f0476a456750aa52
Jeannine Loftus <jmloftus13@yahoo.com>
2017-01-17 19:57:35 -0500

    Ex4.5 added image carousel

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,11 @@
 class StaticPagesController < ApplicationController
   def index
   end
+  def index
+  	@products = Product.limit(3)
+  end
   def landing_page
-  	@featured_product = Product.limit(3)
+  	@featured_product = Product.first
+  	@products = Product.limit(3)
   end  
 end
