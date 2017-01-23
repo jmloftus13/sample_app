2a3213de26608342ec46602e3d73a12ade8da8e4
Jeannine Loftus <jmloftus13@yahoo.com>
2017-01-22 21:23:36 -0500

    Ex5.1 adding dynamic links

--- config/backup of routes.rb
+++ config/backup of routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
   resources :products
   get 'static_pages/about'
-  <%= link_to "About Page", @about %>
 
   get 'static_pages/contact'
-  <%= link_to "Contact Page", @contact %>
 
  root 'static_pages#about' #change back to landing_page later
   get 'static_pages/index' #this statement was in the original file
-  
   get 'static_pages/landing_page'
 
   resources :orders, only: [:index, :show, :create, :detroy]
