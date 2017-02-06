62264fd53bffa472ed19505c424a984d2cb270a1
Jeannine Loftus <jmloftus13@yahoo.com>
2017-02-06 18:06:30 -0500

    Ex5.3 contact form

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,5 +7,5 @@ class StaticPagesController < ApplicationController
   def landing_page
   	@featured_product = Product.first
   	@products = Product.limit(3)
-  end  
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   
   get 'static_pages/landing_page'
 
+  post 'static_pages/thank_you'
+
   resources :orders, only: [:index, :show, :create, :detroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
