13207705013d30df5f5886c31966658ebebeb169
Jeannine Loftus <jmloftus13@yahoo.com>
2017-02-07 18:32:04 -0500

    ex5.3 fixing mailers code in static pages controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -12,16 +12,10 @@ class StaticPagesController < ApplicationController
 	  @name = params[:name]
 	  @email = params[:email]
 	  @message = params[:message]
-	  ActionMailer::Base.mail(:from => @email,
-	      :to => 'your-email@example.com',
+	  UserMailer.contact_form(@email, @name, @message).deliver_now
+	      :to => 'jmloftus13@outlook.com',
 	      :subject => "A new contact form message from #{@name}",
 	      :body => @message).deliver_now
   end
-  def thank_you
-	  @name = params[:name]
-	  @email = params[:email]
-	  @message = params[:message]
-	  UserMailer.contact_form(@email, @name, @message).deliver_now
-	end
 
-end
+end #end StaticPagesController class
