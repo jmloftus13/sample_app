78baf54492049c8d7ba182825afe724a46d677ce
Jeannine Loftus <jmloftus13@yahoo.com>
2017-02-11 16:09:17 -0500

    ex5.5 authorization works

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,7 +3,7 @@ class Ability
 
   def initialize(user) #initialize method
     user ||= User.new  #guest user (not logged in)
-    can :manager, User, id: user.id
+    can :manage, User, id: user.id
   end
 
 end
