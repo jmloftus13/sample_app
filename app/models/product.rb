ec7c5fa654d8b57b8f6e6e79b57940502edde2fc
Jeannine Loftus <jmloftus13@yahoo.com>
2017-02-16 20:34:49 -0500

    ex5.7 Comment model NoMethod error

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,12 @@
 class Product < ApplicationRecord
 	has_many :orders
+	has_many :comments
+
+	def highest_rating_comment
+  		comments.rating_desc.first
+	end
+
+	def lowest_rating_comment
+		comments.rating_asc.first
+	end
 end
