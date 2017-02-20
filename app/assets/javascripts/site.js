6168b02ef2a116e237b5b62ecd25173c7891d0b2
Jeannine Loftus <jmloftus13@yahoo.com>
2017-02-19 19:46:59 -0500

    ex 5.8 testing location change of stars in js file

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,6 +1,6 @@
 $(document).on('turbolinks:load', function(){
-    $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
-    $('.rated').raty({ path: '/assets',
+    $('.rating').raty( { path: '/assets/images', scoreName: 'comment[rating]' });
+    $('.rated').raty({ path: '/assets/images',
       readOnly: true,
       score: function() {
         return $(this).attr('data-score');
