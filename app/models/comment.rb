f972bff2874e1df92de61a198e51495d457375d4
Jeannine Loftus <jmloftus13@yahoo.com>
2017-02-19 21:09:39 -0500

    ex5.9 pagination & validation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,4 +4,9 @@ class Comment < ActiveRecord::Base
 
   scope :rating_desc, -> { order(rating: :desc) }
   scope :rating_asc, -> { order(rating: :asc) }
+
+  validates :body, presence: true
+  	validates :user, presence: true
+  	validates :product, presence: true
+  	validates :rating, numericality: { only_integer: true }
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,8 @@ class Product < ApplicationRecord
 	has_many :orders
 	has_many :comments
 
+	validates :name, presence: true
+
 	def highest_rating_comment
   		comments.rating_desc.first
 	end
