783bb37d88eebc1e092c8c58f5eebaf9bfa6fe03
Jeannine Loftus <jmloftus13@yahoo.com>
2017-03-06 18:26:25 -0500

    ex5.11 trying to fix mailer, still broken

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,17 @@
 class UserMailer < ApplicationMailer
-  default from: "from@example.com"
+  default from: "jmloftus13@yahoo.com"
 
   def contact_form(email, name, message)
-  @message = message
+    @message = message
     mail(:from => email,
         :to => 'jmloftus13@yahoo.com',
         :subject => "A new contact form message from #{name}")
   end
+
+  def send_new_user_message(user)
+    @user = user
+    mail(:to => 'jmloftus13@yahoo.com', 
+        :subject => "New User created please review and enable.")
+  end
+
 end
