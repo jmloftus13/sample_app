04333f74898df57d967a65cb067510b8c7a34b38
Jeannine Loftus <jmloftus13@yahoo.com>
2017-03-12 17:27:00 -0400

    ex 6.14 fixed redis.rb

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,6 @@
+ if Rails.env.production?
+  #commenting out Redis as it caused issues in ex 6.13 & 6.14
+   $redis = Redis.new(url: ENV["REDIS_URL"])
+ else
   $redis = Redis.new(:host => 'localhost', :port => 6379)
+ end 
